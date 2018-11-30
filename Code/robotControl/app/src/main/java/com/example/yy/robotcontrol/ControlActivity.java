3858f86fa8b238183c98f44ba4f4d0704edb5679
Yang Yue <the-chosen-yy@outlook.com>
2018-11-30 21:23:11 +0800

    修改主界面UI 修正下拉列表的校小bug

--- Code/robotControl/app/src/main/java/com/example/yy/robotcontrol/ControlActivity.java
+++ Code/robotControl/app/src/main/java/com/example/yy/robotcontrol/ControlActivity.java
@@ -1,6 +1,8 @@
 package com.example.yy.robotcontrol;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -35,6 +37,16 @@ public class ControlActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.activity_control);
 
         showAllRobotsIcon = (Button) findViewById(R.id.show_all_robots);
